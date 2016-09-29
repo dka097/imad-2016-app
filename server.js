957c2cf0fe43d1429d4f1c20a9c889f763b1d8e0
dka097 <dka097@gmail.com>
2016-09-29 22:28:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function (req, res) {
-    res.send('article one requested and will be served here');
+app.get('/WIN', function (req, res) {
+     res.sendFile(path.join(__dirname, 'ui', 'win.html'));
 }); 
 
 app.get('/article-two', function (req, res) {
