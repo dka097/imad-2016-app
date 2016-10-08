d798d2f010a6dbbeafa88d82ec37df8cf2f6e2f2
dka097 <dka097@gmail.com>
2016-10-08 16:26:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,7 @@
 console.log('Loaded!');
+
+// change the text of the main-text div
+var element = document.getElementById('madi');
+img.oncliick = function () {
+    img.style.marginleft = '100px';
+};  
