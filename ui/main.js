8c6315593f37ae6439b8fa8c8468a4c8ef2db8b2
dka097 <dka097@gmail.com>
2016-10-08 22:59:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,11 +8,11 @@ var button = document.getElementById('counter');
     
      //capture the responds and store it in a variable
      request.onreadystatechange = function() {
-     if ( request.readystate=== XMLhttpRequst.Done) {  //take some action
+     if ( request.readystate === XMLhttpRequst.Done) {  //take some action
      if (request.status === 200) {
-         var counter = request.respooncetext;
-         var span =document.getElementById("count");
-         span .innerHTML = counter.toString();
+         var counter = request.responseText;
+         var span = document.getElementById("count");
+         span.innerHTML = counter.toString();
          
         }
      }
